50f65d5df43ddc8fd2d331bc003db79d80c308de
alemb210 <99274936+alemb210@users.noreply.github.com>
2024-08-23 03:57:56 -0400

    Update GrantMailboxAccess.ps1
    Updated to write to Azure Blob Storage rather than a local CSV. Will verify installation of necessary modules, and install if they are not present.

--- GrantMailboxAccess.ps1
+++ GrantMailboxAccess.ps1
@@ -1,16 +1,54 @@
+# Gather input paramters from user
 $mailbox = Read-Host "Please enter the username of the mailbox owner. (ex: user@yourdomain.com) "
 $admin = Read-Host "Please enter the username of the user being granted mailbox access. (ex: supervisor@yourdomain.com) "
 $days = Read-Host "Please enter the number of days you wish to grant access. "
-$csv = "MockDB.csv" #Path to a CSV file that is used as a database for mailbox access. Does not have to exist prior to runtime.
-if (Get-Module -ListAvailable -Name ExchangeOnlineManagement) { #Check if EO module is installed
-    Write-Host "Module exists"
+
+# Set variables based on current date and given number of days
+$date = "$(Get-Date)"
+$expiration = "$($(Get-Date).AddDays($days))"
+
+# Module verification and installation
+if (Get-Module -ListAvailable -Name ExchangeOnlineManagement) { # Check if EO module is installed
+    Write-Host "EXO module exists"
 } 
 else {
-    Write-Host "Module does not exist. Installing..."
-	Install-Module -Name ExchangeOnlineManagement -Force #Install EO module if needed
+    Write-Host "EXO module does not exist. Installing..."
+	Install-Module -Name ExchangeOnlineManagement -Force # Install EO module if needed
 }
-Connect-ExchangeOnline #Authenticate to EO servers
-Add-MailboxPermission -Identity $mailbox -user $admin -AccessRights FullAccess -Automapping $false #Grant full access to mailbox, do not map it to Outlook.
-$permission = New-Object PsObject -Property @{MailboxOwner = $mailbox ; GrantAccessTo = $admin ; DateSet = Get-Date ; ExpirationDate = (Get-Date).AddDays($days) ; Expired = $false} #Create an array that can be easily exported as a row in CSV
-echo $permission | Select-Object MailboxOwner, GrantAccessTo, DateSet, ExpirationDate, Expired | Export-CSV -Path $csv -Append #Add new row to CSV
-echo "${admin} has access to ${mailbox}'s inbox for ${days} days"
+
+if (Get-Module -ListAvailable -Name Az.Accounts) { # Check if Azure Accounts module is installed
+    Write-Host "AZ Accounts module exists"
+} 
+else {
+    Write-Host "AZ Accounts module does not exist. Installing..."
+	Install-Module -Name Az.Accounts -Force # Install Azure Accounts module if needed
+}
+
+if (Get-Module -ListAvailable -Name Az.Storage) { # Check if Azure Storage module is installed
+    Write-Host "AZ Storage module exists"
+} 
+else {
+    Write-Host "Az Storage module does not exist. Installing..."
+	Install-Module -Name Az.Storage -Force # Install Azure Storage module if needed
+}
+
+# Grant mailbox access
+Connect-ExchangeOnline # Authenticate to EO servers (Will open SSO prompt in browser)
+Add-MailboxPermission -Identity $mailbox -user $admin -AccessRights FullAccess -Automapping $false # Grant full access to mailbox, disable mapping to avoid visual clutter.
+
+# Authenticate to Azure (Will open SSO prompt in browser)
+Connect-AzAccount -Subscription 'Azure Subscription'
+
+# Retrieve CSV storage blob from Azure Storage
+$StorageAccount = Get-AzStorageAccount -ResourceGroupName 'YourResourceGroup' -StorageAccountName 'YourStorageAccount'
+$Context = $StorageAccount.Context
+$ContainerName = 'StorageContainer'
+$Blob = Get-AzStorageBlob -Container $ContainerName -Context $Context
+
+# Load CSV text to memory and modify to reflect new changes
+$Content = $Blob.ICloudBlob.DownloadText()
+$Append = "`n`"$mailbox`",`"$admin`",`"$date`",`"$expiration`",`"$False`""
+$NewContent = $Content + $Append
+
+# Upload modified CSV back to Azure Storage
+$Blob.ICloudBlob.UploadText($NewContent)
